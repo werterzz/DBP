25ce002237a76e099d7a427b8e29f7488c50314b
werterzz <iam.werter@gmail.com>
2019-10-10 15:26:35 +0700

    rearrange Modal

--- public/javascripts/modal.js
+++ public/javascripts/modal.js
@@ -1,8 +1,10 @@
   // Get the modal
   var modal = document.getElementById("myModal");
+  var delEmpMod = document.getElementById("delEmpMod");
   
   // Get the button that opens the modal
   var btn = document.getElementById("myBtn");
+  var delEmpBtn = document.getElementById("delEmpBtn");
   
   // Get the <span> element that closes the modal
   var span = document.getElementsByClassName("close")[0];
@@ -11,10 +13,15 @@
   btn.onclick = function() {
     modal.style.display = "block";
   }
-  
+
+  delEmpBtn.onclick = function() {
+    delEmpMod.style.display = "block";
+  }
+
   // When the user clicks on <span> (x), close the modal
   span.onclick = function() {
     modal.style.display = "none";
+    delEmpMod.style.display = "none";
   }
   
   // When the user clicks anywhere outside of the modal, close it
@@ -22,4 +29,7 @@
     if (event.target == modal) {
       modal.style.display = "none";
     }
+    if (event.target == delEmpMod) {
+        delEmpMod.style.display = "none";
+      }    
   }
