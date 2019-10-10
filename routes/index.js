04cb5e8cd1725d5030b32f25ef99a514703019dc
werterzz <iam.werter@gmail.com>
2019-10-10 13:40:44 +0700

    Send bin file

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const { window } = new JSDOM();
+const { document } = (new JSDOM('')).window;
+global.document = document;
+
+var $ = jQuery = require('jquery')(window);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -8,6 +16,10 @@ router.get('/', function(req, res, next) {
 
 router.get('/employeeInformation', function(req, res, next) {
   res.render('employeeInformation', { title: 'Express' });
+});
+
+router.get('/hello', function(req, res, next) {
+  res.render('hello', { title: 'Express' });
 });
 
 module.exports = router;
