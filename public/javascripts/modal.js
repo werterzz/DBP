ad476c7c90950caedf4e125a5373385d21fc9f45
werterzz <iam.werter@gmail.com>
2019-10-10 23:13:36 +0700

    Add mongodb in employeeInformation

--- public/javascripts/modal.js
+++ public/javascripts/modal.js
@@ -8,6 +8,7 @@
   
   // Get the <span> element that closes the modal
   var span = document.getElementsByClassName("close")[0];
+  var span2 = document.getElementsByClassName("close")[1];
   
   // When the user clicks the button, open the modal 
   btn.onclick = function() {
@@ -21,8 +22,11 @@
   // When the user clicks on <span> (x), close the modal
   span.onclick = function() {
     modal.style.display = "none";
-    delEmpMod.style.display = "none";
   }
+
+  span2.onclick = function() {
+    delEmpMod.style.display = "none";
+  }  
   
   // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
@@ -33,3 +37,16 @@
         delEmpMod.style.display = "none";
       }    
   }
+
+
+var canDel = document.getElementById("canDel");
+
+canDel.onclick = function(event) {
+    delEmpMod.style.display = "none";
+}
+
+var updateEmp = document.getElementById("updateEmp");
+
+updateEmp.onclick = function(event) {
+    modal.style.display = "block";
+}
