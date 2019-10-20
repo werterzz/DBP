4cff26967dc369a291afe6e4dea8a2ef8a18816e
attakorn0798 <flimlnw@hotmail.com>
2019-10-20 23:28:26 +0700

    insert employee and customer

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var methodOverride = require('method-override')
-
-
-
+var customerRouter = require('./routes/customer');
+var employeeRouter = require('./routes/employee');
+var productRouter = require('./routes/product');
 
 
 var app = express();
+var session = require('express-session')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,8 +25,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/customer', customerRouter);
+app.use('/employee', employeeRouter);
+app.use('/product', productRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -36,20 +36,19 @@ router.get('/order', function (req, res, next) {
 
 
 
-
-
 router.get('/hello', function (req, res, next) {
   res.render('hello', { title: 'Express' });
 });
 
 
-router.post('/post/employee', function (req, res, next) {
-  res.redirect("/employeeInformation");
-});
+// router.post('/employeeInformation/add', function (req, res, next) {
+//   res.redirect("/employeeInformation");
+// });
 
-router.post('/del/employee', function (req, res, next) {
-  res.redirect("/employeeInformation");
-});
+
+// router.post('/del/employee', function (req, res, next) {
+//   res.redirect("/employeeInformation");
+// });
 
 router.post('/login', function (req, res, next) {
   MongoClient.connect(url, function (err, db) {
