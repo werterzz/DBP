da3ed47430e9b1e0b199f03aecf15cdc243c09b9
attakorn0798 <flimlnw@hotmail.com>
2019-12-05 01:11:57 +0700

    stock insert

---
+++ models/Products.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+
+const ProductsSchema = new mongoose.Schema({
+    MSRP: {
+        type: String,
+        required: true
+    },
+    buyPrice: {
+        type: String
+    },
+    productCode: {
+        type: String
+    },
+    product: {
+        type: String
+    },
+    contactLastName: {
+        type: String
+    },
+    productDescription: {
+        type: String
+    },
+    productLine: {
+        type: Object
+    },
+    productName: {
+        type: String,
+        required: true
+    },
+    productScale: {
+        type: String
+    },
+    productVendor: {
+        type: String
+    },
+    quantityInStock: {
+        type: String
+    },
+},{ toJSON: { virtuals: true }, toObject: { virtuals: true }});
+
+const StocksSchema = new mongoose.Schema({
+    date: {
+        type: Date,
+        required: true
+    },
+    product: {
+        type: Object       
+    }
+});
+
+ProductsSchema.virtual('stock', {
+    ref: 'Stocks', // The model to use
+    localField: 'product', // Find people where `localField`
+    foreignField: '_id', // is equal to `foreignField`
+    // If `justOne` is true, 'members' will be a single doc as opposed to
+    // an array. `justOne` is false by default.
+    justOne: true,
+    // options: { toObject : {virtuals:true},toJSON : {virtuals : true} } // Query options, see http://bit.ly/mongoose-query-options
+  });
+
+const Stocks = mongoose.model('Stocks', StocksSchema);
+const Products = mongoose.model('Products', ProductsSchema);
+
+module.exports = Products;
--- routes/product.js
+++ routes/product.js
@@ -1,8 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+const Products = require('../models/Products');
 
-router.get('/', function (req, res, next) {
-  res.render('stockProduct', { title: 'Express' });
+router.get('/', function(req, res, next) {
+  res.render("stockProduct", { title: "Stock" });
+});
+
+router.post('/add', function(req, res, next) {
+  console.log("document inserted");
+  let emp = new Products({        
+        MSRP: req.body.msrp,
+        buyPrice: req.body.buyprice,
+        productCode: req.body.productcode,
+        productDescription: req.body.description,
+        productLine: req.body.description,
+        productName: req.body.employeenumber,
+        productScale: req.body.productscale,
+        productVendor: req.body.productvendor,
+        quantityInStock: req.body.quantityinStock})
+        emp.save(function (err) {
+          if (err) console.log(err);
+          // saved!
+          res.redirect("/product");
+        });
+
 });
 
 module.exports = router;
